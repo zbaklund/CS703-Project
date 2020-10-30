c0e9e0f2a28a13ecb71556e7f324bb4acebc4869
Allan <xiaodoubizwx@gmail.com>
2020-10-30 15:34:57 -0500

    finish up the board checking

--- finished.rkt
+++ finished.rkt
@@ -1,41 +1,46 @@
 #lang racket
-
+(require math/array)
 ; a board class
 
 (define soduku% (class object%
-                 (init size)
-                 (define dim size)
-                 (define board (make-vector size (make-vector size -1)))          
-                 (define isFinished "unknown")
-                 (define cols (apply vector-map vector (vector->list board)))
-                 (super-new)
-                 ; methods
-                 (define/public (get-dim)
-                   dim)
-                 (define/public (get-board)
-                   board)
-                 (define/public (get-cols)
-                   cols)
-                 (define/public (get-cell row col)
-                   (vector-ref (vector-ref board row) col))
+                  (init size)
+                  (define dim size)
+                  ; grid size = square root of dimension
+                  (define grid (sqrt size))
+                  (define board (array->mutable-array (index-array (make-vector 2 size))))
+                  ; columns as list of arrays
+                  (define cols (sequence->list (in-array-axis board 1)))
+                  (define rows (sequence->list (in-array-axis board)))
+                  (define isValid "unknown")
+                  (super-new)
+                   ;methods
+                  (define/public (get-dim)
+                    dim)
                   
-                 (define/public (valid-cols)
-                   (let ([valid #t])
-                   (for ([col cols])
+                  (define/public (get-board)
+                    board)
+                  
+                  (define/public (get-cols)
+                    cols)
+                  
+                  (define/public (get-cell row col)
+                    (array-ref board (vector row col)))
+                  
+                  (define/public (set-cell row col val)
+                    (array-set! board (vector row col) val))
+                  
+                  (define/public (valid lines)
+                    (let ([valid #t])
+                    (for ([row lines])
                      #:break (not valid)
-                     (set! valid (and valid (equal? (vector-sort col <) (range dim)))))
-                   valid))
-                 
-                 (define/public (valid-rows)
-                   (let ([valid #t])
-                   (for ([row board])
-                     #:break (not valid)
-                     (set! valid (and valid (equal? (vector-sort row <) (range dim)))))
-                   valid))
+                      (set! valid (and valid (equal? (vector-sort (list->vector (array->list row)) <) (range dim)))))
+                     valid))
 
-                 (define/public (finished)
-                   (if (equal? isFinished "unknown") (and (send this valid-cols) (send this valid-rows)) isFinished))))
+                  (define/public (valid-board)
+                    (if (equal? isValid "unknown") (and (send this valid rows) (send this valid cols)) isValid))))
 
+                   
+            
                    
                 
 
@@ -43,7 +48,10 @@
   
 
 (define b (new soduku% [size 4]))
-(send b valid-cols)
-(send b finished)
-
-                 
+(send b get-board)
+(send b get-cell 2 2)
+(send b set-cell 2 2 -1)
+(send b get-cell 2 2)
+(send b get-board)
+(send b get-cols)
+(send b valid-board)
